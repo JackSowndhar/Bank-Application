12d76db9d753fd7bc5725f467dac465b5b4f629a
Sowndhar M <sowndharsowndhar888@gmail.com>
2025-04-21 15:16:25 +0530

    Update Bank.java

--- BankApplication/BankApplication/src/Bank.java
+++ BankApplication/BankApplication/src/Bank.java
@@ -1,6 +1,6 @@
 public class Bank {
     public static void main(String[] args){
-        System.out.println("$$$ WELCOME TO KANAGARAJ BANK $$$");
+        System.out.println("$$$ WELCOME TO SOWNDHAR BANK $$$");
         new BankUI();
         System.out.println("THANK YOU FOR VISITING...");
     }
